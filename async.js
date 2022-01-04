eea9c0fa6c9f1c0e97ce5f4ee798f69df58e2eac
Priyanka Kumari <priyanka20@navgurukul.org>
2022-01-04 17:19:41 +0530

    Initial commit

---
+++ async.js
@@ -0,0 +1,97 @@
+//  1st questions
+
+// function f1() {
+//     console.log("Bangalore Campus");
+// }
+
+// function f2() {
+//     console.log("Sarjapura Campus");
+//     f1()
+// }
+
+// function f3() {
+//     console.log("Dharamshala Campus");
+//     f2()
+// }
+
+// f3()
+
+
+
+
+
+//  2nd questions
+
+// function fun1() {
+//     console.log("Now start from 6");
+//     for (var i=6; i<=10;i++) {
+//         console.log(i);
+//     }
+// }
+// function fun2(num) {
+//     for(var i=1;i<=5;i++) {
+//         console.log(i);
+//     }
+// }
+// fun2(setTimeout(fun1,3000))
+
+
+
+
+
+//  3rd question
+
+// function add() {
+//     var sum=0;
+//     for (var i=1;i<=10;i++) {
+//         sum = sum + i;
+//     }
+//     console.log(sum);
+// }
+
+// setTimeout(add,2000)
+
+
+
+
+
+//  4th question
+
+// var list1 = [2,5,8,4,9];
+// var list2 = [7,9,5,3,1];
+// function add() {
+// var list3 = []
+// for (var i=0;i<list1.length;i++) {
+//     if (!list2.includes(list1[i])) {
+//         list3.push(list1[i])
+//     }
+// }
+// console.log(list3);
+// }
+
+// setTimeout(add,2000)
+
+
+
+
+
+
+//  5th question
+
+// function table1() {
+//     console.log("Now start from 12");
+//     for (var i=11;i<=20;i++) {
+//         if (i % 2 == 0) {
+//             console.log(i);
+//         }
+//     }
+// }
+
+// function table2() {
+//     for (var i=1;i<=10;i++) {
+//         if (i % 2 == 0) {
+//             console.log(i);
+//         }
+//     }
+// }
+// table2(setTimeout(table1,3000))
